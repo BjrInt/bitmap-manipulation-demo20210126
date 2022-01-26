061c66ea9edfc4a74eb387443ff66f48f87b8d97
BonjourInternet <bonjour@bonjourinternet.top>
2022-01-26 17:28:03 +0100

    [fix] GH pages

--- main.js
+++ main.js
@@ -8,26 +8,28 @@ canvas.width = forestImg.width
 
 const context = canvas.getContext('2d')
 
-// Copy the forest image in the canvas to use it
-context.drawImage(forestImg, 0, 0)
+forestImg.onload(() => {
+  // Copy the forest image in the canvas to use it
+  context.drawImage(forestImg, 0, 0)
 
-// Iterate over the columns of the canvas (by 5px each time)
-for(let i=0; i<canvas.width; i+=5){
-  // Iterate over the rows of the canvas (by 5px each time)
-  for(let j=0; j<canvas.height; j+=5){
+  // Iterate over the columns of the canvas (by 5px each time)
+  for(let i=0; i<canvas.width; i+=5){
+    // Iterate over the rows of the canvas (by 5px each time)
+    for(let j=0; j<canvas.height; j+=5){
 
-    // Get the Red, Green, Blue and opacity value for the current px
-    const data = context.getImageData(i, j, 1, 1).data
+      // Get the Red, Green, Blue and opacity value for the current px
+      const data = context.getImageData(i, j, 1, 1).data
 
-    // To get the luminosity of the pixel we sum every channel (Red/Green/Blue)
-    // and then divide it by 3 to get the mean value
-    const greyLvl = Math.floor(data[0] + data[1] + data[2] / 3)
+      // To get the luminosity of the pixel we sum every channel (Red/Green/Blue)
+      // and then divide it by 3 to get the mean value
+      const greyLvl = Math.floor(data[0] + data[1] + data[2] / 3)
 
-    // We set our "pencil color" to the grey lvl
-    context.fillStyle =`rgb(${greyLvl},${greyLvl},${greyLvl})` 
+      // We set our "pencil color" to the grey lvl
+      context.fillStyle =`rgb(${greyLvl},${greyLvl},${greyLvl})` 
 
-    // We create a 5 by 5 rectangle of the grey color
-    context.fillRect(i, j, 5, 5)
+      // We create a 5 by 5 rectangle of the grey color
+      context.fillRect(i, j, 5, 5)
+    }
   }
-}
+})
 
